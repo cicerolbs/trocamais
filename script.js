8789c049eb34a82ce76b0360520023ef3afb3c52
cicerolbs <cicerolbs@gmail.com>
2023-10-27 17:15:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+const juros = {
+  // ...restante do objeto juros...
+};
+
+function simular() {
+  const bandeira = document.getElementById('bandeira').value;
+  // ...restante do código...
+  const valorParcela = totalPagar / parcelas;
+
+  const resultado = `Valor do empréstimo: R$ ${valorEmprestimo.toFixed(2)}<br>Total a ser pago: R$ ${totalPagar.toFixed(2)}<br>Valor da Parcela: R$ ${valorParcela.toFixed(2)}<br>Quantidade de parcelas: ${parcelas}<br>Juros ao mês: ${jurosMes.toFixed(2)}%<br>Juros ao ano: ${jurosAno.toFixed(2)}%<br>Custo Efetivo Total: ${cet}%<br>Bandeira do cartão: ${bandeira.toUpperCase()}`;
+  document.getElementById('resultado').innerHTML = resultado;
+}
+
+// ...restante do código...
